6c9f3230fb6c3812bc33adeefd79b43dd3723d4d
shafee_azeem <shafeeghani@gmail.com>
2026-02-09 00:03:23 +0530

    firebase Admin update

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -2,13 +2,12 @@ import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 if (!getApps().length) {
+    const serviceAccount = JSON.parse(
+        process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+    );
+
     initializeApp({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-        }),
-        storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`
+        credential: cert(serviceAccount),
     });
 }
 

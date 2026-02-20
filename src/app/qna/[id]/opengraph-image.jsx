44b400f2d108129b14f293ac5a7cd53d5960982b
shafee_azeem <shafeeghani@gmail.com>
2026-02-21 02:22:32 +0530

    opengraph og image

--- src/app/qna/[id]/opengraph-image.jsx
+++ src/app/qna/[id]/opengraph-image.jsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from "next/og";
-import { adminDB } from "@/lib/firebaseAdmin";
 
 export const size = {
     width: 1200,
@@ -8,16 +7,11 @@ export const size = {
 
 export const contentType = "image/png";
 
+// Force Node runtime instead of Edge
+export const runtime = "nodejs";
+
 export default async function Image({ params }) {
-    const doc = await adminDB.collection("qna_items").doc(params.id).get();
-    const data = doc.data();
-
-    const question = data?.question_en || "Al Ansar Weekly";
-
-    // Fetch background from public folder
-    const bg = await fetch(
-        new URL("/default-image/default-image-qna.jpg", process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000")
-    ).then((res) => res.arrayBuffer());
+    const question = "Al Ansar Weekly";
 
     return new ImageResponse(
         (
@@ -25,35 +19,18 @@ export default async function Image({ params }) {
                 style={{
                     width: "100%",
                     height: "100%",
-                    position: "relative",
+                    background: "#0B3D91",
                     display: "flex",
                     alignItems: "center",
                     justifyContent: "center",
+                    padding: "80px",
                     color: "white",
-                    textAlign: "center",
                     fontSize: 48,
                     fontWeight: 600,
+                    textAlign: "center",
                 }}
             >
-                <img
-                    src={bg}
-                    style={{
-                        position: "absolute",
-                        inset: 0,
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
-                />
-
-                <div
-                    style={{
-                        position: "relative",
-                        zIndex: 1,
-                    }}
-                >
-                    {question}
-                </div>
+                {question}
             </div>
         ),
         { ...size }

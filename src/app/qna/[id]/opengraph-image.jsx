ea11a15d0baaeef6b979e92ef30e8b2365c9219d
shafee_azeem <shafeeghani@gmail.com>
2026-02-21 03:34:47 +0530

    Open graph final

--- src/app/qna/[id]/opengraph-image.jsx
+++ src/app/qna/[id]/opengraph-image.jsx
@@ -59,7 +59,6 @@ export default async function Image({ params }) {
                     }}
                 />
 
-                {/* Blue overlay for compression */}
                 <div
                     style={{
                         position: "absolute",

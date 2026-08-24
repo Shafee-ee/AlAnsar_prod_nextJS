1d943ede33995bc1072a8fa1ff3ad88cb711f9bd
Shafee Azeem <shafeeghani@gmail.com>
2026-08-24 14:41:37 +0530

    email IDs enabled

--- src/lib/email.js
+++ src/lib/email.js
@@ -2,11 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const IMAM_NOTIFICATION_LIST = [
-  "shafeeazeemag@gmail.com",
-  "muyeen@technopulse.in",
-  "gmmkamil@gmail.com",
-];
+const IMAM_NOTIFICATION_LIST = ["muyeen@technopulse.in", "gmmkamil@gmail.com"];
 
 export async function sendEmailToImam(question, submissionId) {
   try {

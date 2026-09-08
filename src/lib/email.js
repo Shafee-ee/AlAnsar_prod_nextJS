54c02a239fecca2e285459bbf0303350a84cb7c1
Shafee Azeem <shafeeghani@gmail.com>
2026-09-08 11:00:51 +0530

    translating logic for single upload-done

--- src/lib/email.js
+++ src/lib/email.js
@@ -3,10 +3,10 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const IMAM_NOTIFICATION_LIST = [
-  // "muyeen@technopulse.in",
-  // "gmmkamil@gmail.com",
-  "shafeeazeemag@gmail.com",
-  "shafeeghani@gmail.com",
+  "muyeen@technopulse.in",
+  "gmmkamil@gmail.com",
+  //   "shafeeazeemag@gmail.com",
+  //   "shafeeghani@gmail.com",
 ];
 
 export async function sendEmailToImam({
